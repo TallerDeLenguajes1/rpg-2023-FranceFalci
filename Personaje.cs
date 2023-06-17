81e193fb982445cb4b03f22c6c03a0613682e3f6
Francesca <francescafacia@gmail.com>
2023-06-17 11:37:04 -0300

    creacion clase fabrica de personaje

--- Personaje.cs
+++ Personaje.cs
@@ -2,9 +2,9 @@ namespace EspacioPersonaje;
 
 public class Personaje{
 
-    Tipos tipo;
+    string? tipo;
     string? nombre;
-    string? apodo;
+    // string? apodo;
     DateTime fechaNac;
     int edad;
     int velocidad;
@@ -14,10 +14,48 @@ public class Personaje{
     int armadura;
     int salud;
 
-
+    public string? Tipo { get => tipo; set => tipo = value; }
+    public string? Nombre { get => nombre; set => nombre = value; }
+    // public string? Apodo { get => apodo; set => apodo = value; }
+    public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+    public int Edad { get => edad; set => edad = value; }
+    public int Velocidad { get => velocidad; set => velocidad = value; }
+    public int Destreza { get => destreza; set => destreza = value; }
+    public int Fuerza { get => fuerza; set => fuerza = value; }
+    public int Nivel { get => nivel; set => nivel = value; }
+    public int Armadura { get => armadura; set => armadura = value; }
+    public int Salud { get => salud; set => salud = value; }
 }
 
-public enum Tipos{
-    vivo,
-    muerto
+
+
+public class FabricaDePersonajes{
+     
+    string[] nombres = {"Liu Kang","Escorpion","Goro", "Subzero", "Raiden"};
+    string[] tipos = {"vivo","muerto"};
+
+    Random random = new Random();
+
+
+    public Personaje crearPersonaje(){
+        int anio = random.Next(1900, 2024);
+        int mes = random.Next(1, 13);
+        int dia = random.Next(1, DateTime.DaysInMonth(anio, mes) + 1);
+        DateTime hoy =   DateTime.Today;
+        Personaje nuevoPersonaje = new();
+        nuevoPersonaje.Tipo = tipos[random.Next(0,1)];// revisar
+        nuevoPersonaje.Nombre = nombres[random.Next(0,5)];
+        nuevoPersonaje.Armadura = random.Next(1,11);
+        nuevoPersonaje.Fuerza = random.Next(1,11);
+        nuevoPersonaje.Destreza = random.Next(1,6);
+        nuevoPersonaje.Nivel = random.Next(1,11);
+        nuevoPersonaje.Velocidad = random.Next(1,11);
+        nuevoPersonaje.FechaNac =new DateTime(anio, mes, dia);
+        nuevoPersonaje.Edad= DateTime.Today.Subtract(nuevoPersonaje.FechaNac).Days / 365;
+        nuevoPersonaje.Salud = 100;
+
+
+        return nuevoPersonaje;
+        
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿internal class Program
+﻿using EspacioPersonaje;
+internal class Program
 {
     private static void Main(string[] args)
     {
-    }
+        Personaje nuevo;
+        nuevo = new FabricaDePersonajes().crearPersonaje();
+        Console.WriteLine(nuevo.Edad);
+        Console.WriteLine(nuevo.Nombre);
+        Console.WriteLine(nuevo.Tipo);
+
+
+    } 
 }
